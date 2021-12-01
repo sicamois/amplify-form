4c60d6abec483619fac9be6a334bcd0218efcc7a
Po Sicamois <sicamois@gmail.com>
2021-12-01 12:32:51 +0100

    new build

--- dist/lib/index.d.ts
+++ dist/lib/index.d.ts
@@ -3,4 +3,4 @@ import { FileWithStorageKey } from "./components/AmplifyForm";
 import { FormValues } from './components/FormComponent';
 export { AmplifyForm };
 export { AmplifyForm as default };
-export type { FileWithStorageKey, FormValues };
+export { FileWithStorageKey, FormValues };
--- dist/lib/index.js
+++ dist/lib/index.js
@@ -1,5 +1,7 @@
 "use strict";
 
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
@@ -9,6 +11,18 @@ Object.defineProperty(exports, "AmplifyForm", {
     return _AmplifyForm.default;
   }
 });
+Object.defineProperty(exports, "FileWithStorageKey", {
+  enumerable: true,
+  get: function get() {
+    return _AmplifyForm.FileWithStorageKey;
+  }
+});
+Object.defineProperty(exports, "FormValues", {
+  enumerable: true,
+  get: function get() {
+    return _FormComponent.FormValues;
+  }
+});
 Object.defineProperty(exports, "default", {
   enumerable: true,
   get: function get() {
@@ -16,7 +30,11 @@ Object.defineProperty(exports, "default", {
   }
 });
 
-var _AmplifyForm = _interopRequireDefault(require("./components/AmplifyForm"));
+var _AmplifyForm = _interopRequireWildcard(require("./components/AmplifyForm"));
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9saWIvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7Ozs7Ozs7Ozs7O0FBQUEiLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgQW1wbGlmeUZvcm0gZnJvbSBcIi4vY29tcG9uZW50cy9BbXBsaWZ5Rm9ybVwiO1xuaW1wb3J0IHsgRmlsZVdpdGhTdG9yYWdlS2V5IH0gZnJvbSBcIi4vY29tcG9uZW50cy9BbXBsaWZ5Rm9ybVwiXG5pbXBvcnQgeyBGb3JtVmFsdWVzIH0gZnJvbSAnLi9jb21wb25lbnRzL0Zvcm1Db21wb25lbnQnO1xuXG5leHBvcnQgeyBBbXBsaWZ5Rm9ybSB9IFxuZXhwb3J0IHsgQW1wbGlmeUZvcm0gYXMgZGVmYXVsdH0gXG5leHBvcnQgdHlwZSB7IEZpbGVXaXRoU3RvcmFnZUtleSwgRm9ybVZhbHVlcyB9Il19
+var _FormComponent = require("./components/FormComponent");
+
+function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
+
+function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9saWIvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7QUFBQTs7QUFFQSIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCBBbXBsaWZ5Rm9ybSBmcm9tIFwiLi9jb21wb25lbnRzL0FtcGxpZnlGb3JtXCI7XG5pbXBvcnQgeyBGaWxlV2l0aFN0b3JhZ2VLZXkgfSBmcm9tIFwiLi9jb21wb25lbnRzL0FtcGxpZnlGb3JtXCJcbmltcG9ydCB7IEZvcm1WYWx1ZXMgfSBmcm9tICcuL2NvbXBvbmVudHMvRm9ybUNvbXBvbmVudCc7XG5cbmV4cG9ydCB7IEFtcGxpZnlGb3JtIH0gXG5leHBvcnQgeyBBbXBsaWZ5Rm9ybSBhcyBkZWZhdWx0fSBcbmV4cG9ydCB7IEZpbGVXaXRoU3RvcmFnZUtleSwgRm9ybVZhbHVlcyB9Il19
