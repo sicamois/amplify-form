f10ff4645fdd2124e59dabe2199288a9da9b9540
Po Sicamois <sicamois@gmail.com>
2021-12-01 15:28:16 +0100

    test

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <body>
+        <h1 className='text-xl text-center font-black'>Test AmplifyForm</h1>
+      </body>
     </div>
   );
 }
