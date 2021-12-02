5abaa3dd3d9d58ab699f0db02062b7f62102634b
Po Sicamois <sicamois@gmail.com>
2021-12-02 21:59:38 +0100

    add custom width for FieldSize

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
   purge: ["src/components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        'sm': '5.25rem',
+        '2xl': '30rem',
+        '3xl': '50rem',
+        'screen': '90vw'
+      }
+    },
   },
   variants: {
     extend: {},
