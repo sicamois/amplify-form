6f0649b706d78b64eda9adc492009b34af474b5c
Po Sicamois <sicamois@gmail.com>
2021-12-03 11:29:28 +0100

    fix filename test css

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
         exclude: /node_modules/,
       },
       {
-        test: /\.css/,
+        test: /\.css$/,
         use: ["style-loader", "css-loader", "postcss-loader"],
       },
     ],
