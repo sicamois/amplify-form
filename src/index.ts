60734b1ae8b42b8108529d1c4d2a6bb85cc3ee7a
Po Sicamois <sicamois@gmail.com>
2021-12-07 21:18:49 +0100

    z

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
 import React from 'react'
-import { AmplifyFormProps, FormValues, FileWithStorageKey } from './helpers/types';
 import './index.css';
 import AmplifyForm from './components/AmplifyForm';
 
-export { AmplifyForm };
 export { AmplifyForm as default };
-export type { FileWithStorageKey, FormValues, AmplifyFormProps };
