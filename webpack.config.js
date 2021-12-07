ebf80ef74236c9a1cb62abe91f9a0ee79f1d5bb7
Po Sicamois <sicamois@gmail.com>
2021-12-07 22:53:44 +0100

    c

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
       {
         test: /\.css$/,
         use: [
-          "style-loader",
+          isProductionMode ? MiniCssExtractPlugin.loader : "style-loader",
           {
             loader: "css-loader",
             options: {
