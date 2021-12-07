a5534e8f309bb01fb0951ff2ae9d67c2cbaeb8f6
Po Sicamois <sicamois@gmail.com>
2021-12-07 22:47:57 +0100

    a

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
             options: {
               // Run `postcss-loader` on each CSS `@import` and CSS modules/ICSS imports, do not forget that `sass-loader` compile non CSS `@import`'s into a single file
               // If you need run `sass-loader` and `postcss-loader` on each CSS `@import` please set it to `2`
-              importLoaders: 2,
+              importLoaders: 1,
             },
           },
           {
