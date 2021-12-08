c144ca51fb77993b33b9c8b6451554123d438416
Po Sicamois <sicamois@gmail.com>
2021-12-08 10:00:53 +0100

    s

--- src/components/FormElements/FormElements.tsx
+++ src/components/FormElements/FormElements.tsx
@@ -77,7 +77,7 @@ const FieldWithError: FC<FieldProps> = ({
 export const FieldSet: FC<FieldProps> = ({ label, theme, children }) => {
   return (
     <Fragment>
-      <br />
+      <p><br /></p>
       <fieldset className={theme?.fieldSetStyle || fieldSetStyle}>
         <legend className={theme?.legendStyle || legendStyle}>{label}</legend>
         {children}
