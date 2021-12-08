de8a1616d0870d5084f39bc548339c1f8dcf1b83
Po Sicamois <sicamois@gmail.com>
2021-12-08 17:16:49 +0100

    f

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,8 @@
-import autoprefixer from 'autoprefixer';
-import tailwindcss from 'tailwindcss';
-
 module.exports = {
-  plugins: [
-    tailwindcss(),
-    autoprefixer(),
-  ],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
   sourceMap: true,
   extract: true,
   minimize: true
