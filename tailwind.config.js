1647dde12ce710abb31cbf3c3d0451c2aa647413
Po Sicamois <sicamois@gmail.com>
2021-12-09 20:22:07 +0100

    add width extensions

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,14 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        '20': '5.25rem',
+        '100': '30rem',
+        '200': '50rem',
+        'screen': '90vw'
+      }
+    },
   },
   variants: {
     extend: {},
