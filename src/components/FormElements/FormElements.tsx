48634b9d263c56f7bbdc497881e3c9ddaf7bf09f
Po Sicamois <sicamois@gmail.com>
2021-12-10 11:30:46 +0100

    change color

--- src/components/FormElements/FormElements.tsx
+++ src/components/FormElements/FormElements.tsx
@@ -17,7 +17,7 @@ const legendStyle = 'font-black text-lg px-2';
 const labelStyle =
   'text-gray-400 px-2 absolute left-0 -top-3.5 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-sm';
 const fieldStyle =
-  'bg-transparent border-gray-300 text-gray-900 peer h-10 border-0 border-b-2 placeholder-transparent focus:outline-none focus:ring-0';
+  'bg-transparent border-gray-300 text-gray-600 peer h-10 border-0 border-b-2 placeholder-transparent focus:outline-none focus:ring-0';
 const checkboxStyle = 'bg-transparent border-gray-300 rounded border-2 w-5 h-5 focus:ring-0';
 const multiSelectStyle = 'border-gray-300 border-0 border-b-2 relative';
 const errorStyle = 'text-red-700 text-xs';
@@ -40,55 +40,55 @@ export const fieldSizeMap: Map<string, string> = new Map([
 export const textColorMap: Map<string | undefined, string> = new Map([
   ['black', 'text-black'],
   ['white', 'text-white'],
-  ['gray', 'text-gray-900'],
-  ['red', 'text-red-900'],
-  ['blue', 'text-blue-900'],
-  ['yellow', 'text-yellow-900'],
-  ['green', 'text-green-900'],
-  ['indigo', 'text-indigo-900'],
-  ['purple', 'text-purple-900'],
-  ['pink', 'text-pink-900'],
+  ['gray', 'text-gray-600'],
+  ['red', 'text-red-600'],
+  ['blue', 'text-blue-600'],
+  ['yellow', 'text-yellow-600'],
+  ['green', 'text-green-600'],
+  ['indigo', 'text-indigo-600'],
+  ['purple', 'text-purple-600'],
+  ['pink', 'text-pink-600'],
   [undefined, 'text-red-900'],
 ]);
 
 export const peerFocusTextColorMap: Map<string | undefined, string> = new Map([
   ['black', 'peer-focus:text-black'],
   ['white', 'peer-focus:text-white'],
-  ['gray', 'peer-focus:text-gray-900'],
-  ['red', 'peer-focus:text-red-900'],
-  ['blue', 'peer-focus:text-blue-900'],
-  ['yellow', 'peer-focus:text-yellow-900'],
-  ['green', 'peer-focus:text-green-900'],
-  ['indigo', 'peer-focus:text-indigo-900'],
-  ['purple', 'peer-focus:text-purple-900'],
-  ['pink', 'peer-focus:text-pink-900'],
+  ['gray', 'peer-focus:text-gray-600'],
+  ['red', 'peer-focus:text-red-600'],
+  ['blue', 'peer-focus:text-blue-600'],
+  ['yellow', 'peer-focus:text-yellow-600'],
+  ['green', 'peer-focus:text-green-600'],
+  ['indigo', 'peer-focus:text-indigo-600'],
+  ['purple', 'peer-focus:text-purple-600'],
+  ['pink', 'peer-focus:text-pink-600'],
   [undefined, 'peer-focus:text-red-900'],
 ]);
 
 export const focusBorderColorMap: Map<string | undefined, string> = new Map([
   ['black', 'focus:border-black'],
   ['white', 'focus:border-white'],
-  ['gray', 'focus:border-gray-900'],
-  ['red', 'focus:border-red-900'],
-  ['blue', 'focus:border-blue-900'],
-  ['yellow', 'focus:border-yellow-900'],
-  ['green', 'focus:border-green-900'],
-  ['indigo', 'focus:border-indigo-900'],
-  ['purple', 'focus:border-purple-900'],
-  ['pink', 'focus:border-pink-900'],
+  ['gray', 'focus:border-gray-600'],
+  ['red', 'focus:border-red-600'],
+  ['blue', 'focus:border-blue-600'],
+  ['yellow', 'focus:border-yellow-600'],
+  ['green', 'focus:border-green-600'],
+  ['indigo', 'focus:border-indigo-600'],
+  ['purple', 'focus:border-purple-600'],
+  ['pink', 'focus:border-pink-600'],
   [undefined, 'focus:border-red-900'],
 ]);
 
 export const bgColorMap: Map<string | undefined, string> = new Map([
   ['black', 'bg-black'],
-  ['gray', 'bg-gray-900'],
-  ['red', 'bg-red-900'],
-  ['blue', 'bg-blue-900'],
-  ['yellow', 'bg-yellow-900'],
-  ['green', 'bg-green-900'],
-  ['indigo', 'bg-indigo-900'],
-  ['purple', 'bg-purple-900'],
-  ['pink', 'bg-pink-900'],
+  ['gray', 'bg-gray-600'],
+  ['red', 'bg-red-600'],
+  ['blue', 'bg-blue-600'],
+  ['yellow', 'bg-yellow-600'],
+  ['green', 'bg-green-600'],
+  ['indigo', 'bg-indigo-600'],
+  ['purple', 'bg-purple-600'],
+  ['pink', 'bg-pink-600'],
   [undefined, 'bg-red-900'],
 ]);
 
