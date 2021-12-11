c9f422648098d9af7c00659119d4bef88a913ed7
Po Sicamois <sicamois@gmail.com>
2021-12-11 09:42:35 +0100

    fix build warnings

--- tsdx.config.js
+++ tsdx.config.js
@@ -1,9 +1,18 @@
 const postcss = require('rollup-plugin-postcss');
+const replace = require('@rollup/plugin-replace');
 
 module.exports = {
-  rollup(config, opts) {
+  rollup(config, options) {
+    config.plugins = config.plugins.map(plugin =>
+      plugin.name === 'replace'
+        ? replace({
+            'process.env.NODE_ENV': JSON.stringify(options.env),
+            preventAssignment: true,
+          })
+        : plugin
+    );
     config.plugins.push(
-      postcss()
+      postcss({ extract: true })
     );
     // if (opts.format === 'esm') {
     //   config = { ...config, preserveModules: true }
