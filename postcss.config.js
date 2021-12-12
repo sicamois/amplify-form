94af0796416b4e814173c0f649c6ea4fb88b59e4
Po Sicamois <sicamois@gmail.com>
2021-12-12 11:19:33 +0100

    back to init config

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,6 @@
-// If you want to use other PostCSS plugins, see the following:
-// https://tailwindcss.com/docs/using-with-preprocessors
 module.exports = {
-  plugins: [
-    // require('postcss-import'),
-    require('tailwindcss'),
-    require('autoprefixer'),
-    // require('cssnano')({
-    //   preset: 'default',
-    // }),
-  ],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  }
 }
