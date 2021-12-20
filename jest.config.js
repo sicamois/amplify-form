b64cfb6426294eeca1e069fa704660a1fa53c82f
Po Sicamois <sicamois@gmail.com>
2021-12-20 01:53:50 +0100

    simplify jest setup

--- jest.config.js
+++ jest.config.js
@@ -7,8 +7,17 @@ const createJestConfig = nextJest({
 
 // Add any custom config to be passed to Jest
 const customJestConfig = {
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  testEnvironment: "jsdom",
+  // Jest transformations -- this adds support for TypeScript
+  // using ts-jest
+  transform: {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  setupFilesAfterEnv: [
+    '@testing-library/jest-dom/extend-expect',
+  ],
+  testEnvironment: "jest-environment-jsdom",
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
