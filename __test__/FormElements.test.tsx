8bf839b983dff0e9038c81be0f99a9bbe3091524
Po Sicamois <sicamois@gmail.com>
2021-12-20 16:23:39 +0100

    add tests

--- __test__/FormElements.test.tsx
+++ __test__/FormElements.test.tsx
@@ -1,14 +1,17 @@
-import { Form, Formik, FormikProps } from 'formik';
+import { Form, Formik } from 'formik';
 import { FC } from 'react';
 import { create } from 'react-test-renderer';
 import { render, screen } from '@testing-library/react';
 import {
   CheckboxField,
+  FieldSet,
   NumberField,
   TextAreaField,
   TextField,
 } from '../src/components/FormElements';
 import { FieldProps, FormTheme } from '../src/types';
+import { capitalize } from 'lodash';
+import { FieldWithError } from '../src/components/FormElements/FormElements';
 
 const TestForm: FC<{ initialValues: Record<string, any> }> = ({
   initialValues,
@@ -23,6 +26,136 @@ const TestForm: FC<{ initialValues: Record<string, any> }> = ({
     </Formik>
   );
 };
+
+describe('FieldWithError', () => {
+  let spy: jest.SpyInstance;
+  let spyError: jest.SpyInstance;
+  let spyWarn: jest.SpyInstance;
+  beforeAll(() => {
+    spy = jest.spyOn(console, 'log').mockImplementation(() => {});
+    spyError = jest.spyOn(console, 'error').mockImplementation(() => {});
+    spyWarn = jest.spyOn(console, 'warn').mockImplementation(() => {});
+  });
+  afterAll(() => {
+    spy.mockRestore();
+    spyError.mockRestore();
+    spyWarn.mockRestore();
+  });
+
+  it('renders correctly with label', () => {
+    const name = 'set';
+    const label = 'Infos';
+    const props: FieldProps = {
+      name,
+      label,
+    };
+    const tree = create(
+      <TestForm initialValues={{}}>
+        <FieldWithError {...props}>
+          <div></div>
+        </FieldWithError>
+      </TestForm>
+    ).toJSON();
+    expect(tree).not.toBeNull();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders correctly without label', () => {
+    const name = 'set';
+    const props: FieldProps = {
+      name,
+    };
+    const tree = create(
+      <TestForm initialValues={{}}>
+        <FieldWithError {...props}>
+          <div></div>
+        </FieldWithError>
+      </TestForm>
+    ).toJSON();
+    expect(tree).not.toBeNull();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('sets field size correctly when prop is passed', () => {
+    const name = 'set';
+    const label = 'Infos';
+    const props: FieldProps = {
+      name,
+      label,
+      fieldSize: 'md',
+    };
+    render(
+      <TestForm initialValues={{ title: '' }}>
+        <FieldWithError {...props} />
+      </TestForm>
+    );
+    const labelElement = screen.getByText(label);
+    expect(labelElement.classList).toContain('w-36');
+    expect(labelElement.classList).not.toContain('w-full');
+  });
+
+  it('set field size to default when prop is set incorrectly', () => {
+    const name = 'set';
+    const label = 'Infos';
+    const props: FieldProps = {
+      name,
+      label,
+      fieldSize: 'xxx',
+    };
+    render(
+      <TestForm initialValues={{ title: '' }}>
+        <FieldWithError {...props} />
+      </TestForm>
+    );
+    const labelElement = screen.getByText(label);
+    expect(labelElement.classList).toContain('w-full');
+  });
+
+  it('sets theme correctly when prop is passed', () => {
+    const name = 'set';
+    const label = 'Infos';
+    const theme: FormTheme = { color: 'lime' };
+    const props: FieldProps = {
+      name,
+      label,
+      theme,
+    };
+    render(
+      <TestForm initialValues={{}}>
+        <FieldWithError {...props} />
+      </TestForm>
+    );
+    const labelElement = screen.getByText(label);
+    expect(labelElement.classList).toContain('peer-focus:text-lime-600');
+    expect(labelElement.classList).not.toContain('peer-focus:text-red-900');
+  });
+
+  it('sets labelCentered correctly when prop is passed', () => {
+    const name = 'set';
+    const label = 'Infos';
+    const props: FieldProps = {
+      name,
+      label,
+    };
+    const renderResult = render(
+      <TestForm initialValues={{}}>
+        <FieldWithError {...props} />
+      </TestForm>
+    );
+    const labelElement = screen.getByText(label);
+    expect(labelElement.classList).not.toContain('text-center');
+
+    renderResult.unmount();
+    props.labelCentered = true;
+    render(
+      <TestForm initialValues={{}}>
+        <FieldWithError {...props} />
+      </TestForm>
+    );
+    const labelCenteredElement = screen.getByText(label);
+    expect(labelCenteredElement.classList).toContain('text-center');
+  });
+});
 
 describe('TextField', () => {
   let spy: jest.SpyInstance;
@@ -344,5 +477,99 @@ describe('CheckboxField', () => {
     const inputElement = screen.getByRole('checkbox');
     expect(inputElement.classList).toContain('accent-lime-600');
     expect(inputElement.classList).not.toContain('accent-red-900');
+  });
+});
+
+describe('FieldSet', () => {
+  let spy: jest.SpyInstance;
+  let spyError: jest.SpyInstance;
+  let spyWarn: jest.SpyInstance;
+  beforeAll(() => {
+    spy = jest.spyOn(console, 'log').mockImplementation(() => {});
+    spyError = jest.spyOn(console, 'error').mockImplementation(() => {});
+    spyWarn = jest.spyOn(console, 'warn').mockImplementation(() => {});
+  });
+  afterAll(() => {
+    spy.mockRestore();
+    spyError.mockRestore();
+    spyWarn.mockRestore();
+  });
+
+  it('renders correctly', () => {
+    const name = 'set';
+    const label = 'Infos';
+    const props: FieldProps = {
+      name,
+      label,
+    };
+    const tree = create(
+      <TestForm initialValues={{}}>
+        <FieldSet {...props}>
+          <div></div>
+        </FieldSet>
+      </TestForm>
+    ).toJSON();
+    expect(tree).not.toBeNull();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('defaults label to name when no label is passed', () => {
+    const name = 'field_set';
+    const props: FieldProps = {
+      name,
+    };
+    render(
+      <TestForm initialValues={{ ref: '' }}>
+        <FieldSet {...props} />
+      </TestForm>
+    );
+    const legendElement = screen.getByTestId(
+      `${name}-legend`
+    ) as HTMLLegendElement;
+    expect(legendElement.textContent).toBe(
+      capitalize(name).replaceAll('_', ' ')
+    );
+  });
+
+  it('sets basic theme correctly when prop is passed', () => {
+    const name = 'set';
+    const label = 'Infos';
+    const theme: FormTheme = { color: 'lime' };
+    const props: FieldProps = {
+      name,
+      label,
+      theme,
+    };
+    render(
+      <TestForm initialValues={{ ref: '' }}>
+        <FieldSet {...props} />
+      </TestForm>
+    );
+    const fieldsetElement = screen.getByRole('group');
+    const legendElement = screen.getByTestId(`${name}-legend`);
+    expect(fieldsetElement.classList).not.toContain('border-lime-600');
+    expect(legendElement.classList).toContain('text-lime-600');
+    expect(legendElement.classList).not.toContain('text-red-900');
+  });
+
+  it('sets full theme correctly when prop is passed', () => {
+    const name = 'set';
+    const label = 'Infos';
+    const theme: FormTheme = { color: 'lime', branding: 'full' };
+    const props: FieldProps = {
+      name,
+      label,
+      theme,
+    };
+    render(
+      <TestForm initialValues={{ ref: '' }}>
+        <FieldSet {...props} />
+      </TestForm>
+    );
+    const fieldsetElement = screen.getByRole('group');
+    const legendElement = screen.getByTestId(`${name}-legend`);
+    expect(fieldsetElement.classList).toContain('border-lime-600');
+    expect(legendElement.classList).toContain('text-lime-600');
+    expect(legendElement.classList).not.toContain('text-red-900');
   });
 });
