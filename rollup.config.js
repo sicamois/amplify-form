31ecd0681daa50bb93148a652c3834ee27bda921
Po Sicamois <sicamois@gmail.com>
2021-12-28 16:23:40 +0100

    fix rollup build process

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,50 @@
 import esbuild from 'rollup-plugin-esbuild';
 import postcss from 'rollup-plugin-postcss';
+import packageJson from './package.json';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+
+const devDependencies = {
+  ...packageJson.devDependencies,
+};
+const dependencies = {
+    ...packageJson.dependencies,
+  };
 
 export default {
     input: 'src/index.ts',
-    output: {
-        file: 'dist/index.js',
-        format: 'esm'
-    },
+    output: [
+        {
+          file: packageJson.main,
+          format: 'cjs', // commonJS
+          sourcemap: process.env.NODE_ENV !== 'production',
+          exports: 'named',
+        },
+        {
+          file: packageJson.module,
+          format: 'esm', // ES Modules
+          sourcemap: process.env.NODE_ENV !== 'production',
+        },
+      ],
     plugins: [
-        esbuild({ tsconfig: 'tsconfig.build.json' }),
+        peerDepsExternal(),
+        nodeResolve(),
+        commonjs({
+            exclude: 'node_modules',
+            ignoreGlobal: true,
+        }),
+        esbuild({ 
+            tsconfig: 'tsconfig.build.json',
+            minify: process.env.NODE_ENV === 'production',
+            target: 'ESNext',
+            optimizeDeps: {
+                include: Object.keys(dependencies),
+            },
+        }),
         postcss({
             plugins: []
-          }),
-    ]
+        }),
+    ],
+  external: Object.keys(devDependencies),
 };
