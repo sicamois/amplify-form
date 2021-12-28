6e6337f0a6a9ee635bad2029ceb1df6c4d227407
Po Sicamois <sicamois@gmail.com>
2021-12-28 18:38:34 +0100

    add style-inject and build w/ tsc

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
-import './styles/globals.css';
+// @ts-ignore
+import styleInject from 'style-inject';
+// @ts-ignore
+import css from './styles/globals.css';
+styleInject(css);
 export { default } from './components/AmplifyForm/AmplifyForm';
 export * from './types';
